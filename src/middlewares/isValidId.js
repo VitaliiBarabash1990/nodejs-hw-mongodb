baae66ee07ec66a1f9ffee6991cc9f8e8e0a068b
vitalii-pc <alievruslan1818@gmail.com>
2024-12-02 12:42:25 +0100

    corect_isValid

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,8 +2,8 @@ import createHttpError from 'http-errors';
 import { isValidObjectId } from 'mongoose';
 
 export const isValidId = (req, res, next) => {
-  const { studentId } = req.params;
-  if (!isValidObjectId(studentId)) {
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
     throw createHttpError(400, 'Bad Request');
   }
   next();
