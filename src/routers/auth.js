58bdab543c1b2f67aab365a7a81361bcdec2d592
vitalii-pc <alievruslan1818@gmail.com>
2024-12-10 18:02:51 +0100

    hw5-auth

---
+++ src/routers/auth.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import {
+  loginUserController,
+  logoutUserController,
+  refreshUserSessionController,
+  registerUserController,
+} from '../controllers/auth.js';
+import { validateBody } from '../middlewares/validateBody.js';
+
+const router = express.Router();
+const jsonParser = express.json();
+
+router.post(
+  '/register',
+  jsonParser,
+  validateBody(registerUserSchema),
+  ctrlWrapper(registerUserController),
+);
+router.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
+);
+router.post('/logout', ctrlWrapper(logoutUserController));
+
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+
+export default router;
